11223e01aa6d974dd34937ad859475ba5b42510e
Sa_Sek <sasek2019@gmail.com>
2023-11-07 19:23:09 +0700

    add dialog

--- src/app/pages/webpages/home-page/home-page.component.ts
+++ src/app/pages/webpages/home-page/home-page.component.ts
@@ -10,6 +10,8 @@ export class HomePageComponent implements OnInit {
 
   loading: boolean = true;
   countries: any[] = [];
+  display: boolean = false;
+  country: any = {};
 
   constructor(
     private $resCountry: RestcountriesService
@@ -22,13 +24,20 @@ export class HomePageComponent implements OnInit {
   async getPageInfo() {
     this.loading = true;
     try {
-      this.countries = await this.$resCountry.getCoutries({},'/all');
-      console.log("countries", this.countries);      
-      console.log("Name", this.countries[0].name.official);      
+      this.countries = await this.$resCountry.getCoutries({}, '/all');
+      console.log("countries", this.countries);
+      console.log("Name", this.countries[0].name.official);
       this.loading = false;
     } catch (ex) {
       this.loading = false;
     }
+  }
+
+  showDialog(value: {}) {
+    this.display = true;
+    this.country = value;
+    console.log("country", this.country);
+    
   }
 
 }
